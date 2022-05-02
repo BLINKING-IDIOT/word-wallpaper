4dec8d85d2936048dd96deb2d73675f4eeaa291e
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2022-05-02 12:45:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,27 @@
 !/bin/bash
 clear
-echo 
-echo
+printf "\n\n"
 if [[ $(id -u) -ne 0 ]] ; then
 	mkdir ~/.config/autostart
 	cp word-wallpaper.desktop ~/.config/autostart/word-wallpaper.desktop
 	clear
 	echo "you need root assess to install necessary packages"
-	echo
+	printf "\n"
 	echo "so please enter the password to login as root!"
-	echo
+	printf "\n"
 	sudo bash ${0}
-	echo 
-	echo 
+	printf "\n\n"
 	exit
 fi
 echo "NOTE: you need internet connction to install necessary packages"
 echo "so please make sure computer is connected to internet"
-echo
-echo
+printf "\n\n"
 for i in 3 2 1
 do
 	echo "staring installation process in ${i}" ; 
 	sleep 1
 done
-echo 
-echo 
+printf "\n\n" 
 apt update -y
 pip3 install pynput
 FILE1=/usr/bin/word-wallpaper
@@ -36,8 +32,7 @@ cp word-wallpaper.py /usr/bin/word-wallpaper
 mkdir /usr/share/word-wallpaper
 cp black.jpg /usr/share/word-wallpaper/black.jpg
 chmod +x /usr/bin/word-wallpaper
-echo 
-echo
+printf "\n\n"
 echo "installed successfully!"
-echo
+printf "\n"
 echo "now relogin"
